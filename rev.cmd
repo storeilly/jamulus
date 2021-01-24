3cc042eb43046cad0eed6b22889c17399851a267
Stephen O Reilly <soreilly64@gmail.com>
2021-01-24 22:07:23 +0000

    fix rev2

--- rev.cmd
+++ rev.cmd
@@ -1,9 +1,4 @@
 @echo off
-echo rev-parse
 git rev-parse --short HEAD
-
-echo describe
 git describe --dirty=-dev --always
-
-echo match
 git describe --match=xxxxxxxxxxxxxxxxxxxx --always --abbrev --dirty
